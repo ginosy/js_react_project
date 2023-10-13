22ee87b7f06d2092f9b81449be123fe444b0aca6
ginosy <137649079+ginosy@users.noreply.github.com>
2023-10-12 17:09:05 -0700

    App.js file

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
+import { AddTodo } from "./components/AddTodo";
+import { TodoLists } from "./components/TodoLists";
+import { Navbar } from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="container p-4 mt-2">
+      <h2>Todo Application</h2>
+      <Router basename={"/Todolist_React_Redux"}>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<About />} />
+          <Route
+            path="/todo"
+            element={
+              <div>
+                <AddTodo />
+                <TodoLists />
+              </div>
+            }
+          />
+          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/projects" element={<Projects />} /> */}
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
