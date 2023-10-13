1598cae850f736a6018ddbbf928eb6511009f0cf
ginosy <137649079+ginosy@users.noreply.github.com>
2023-10-12 17:07:42 -0700

    updated navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item nav-link active">
+              <NavLink class="nav-link active" aria-current="page" to="/">
+                About
+              </NavLink>
+            </li>
+            <li class="nav-item nav-link">
+              <NavLink class="nav-link" to="/todo">
+                Todo
+              </NavLink>
+            </li>
+            <li class="nav-item nav-link">
+              <NavLink class="nav-link" to="/contact">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
