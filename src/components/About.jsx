8e7af863355c51a8ea62fa73e17356420b700d9d
ginosy <ginosy20@gmail.com>
2023-11-07 06:50:50 -0600

    remove inlinestyle

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div style={{margin : "30px"}}>
+    <div className='mt-3'>
         <h4>This is React + Redux Todo Application</h4>
         <h5>Gino Sy</h5>
     </div>
